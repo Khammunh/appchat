8f32512e42b268cf39530951ce94d254a094b761
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-07-29 20:37:36 +0700

    Chat Screen .chat.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:app/screens/auth.dart';
+import 'package:app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Chat',
       theme: ThemeData().copyWith(
         useMaterial3: true,
@@ -17,8 +29,15 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
-    //  home: ...,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
-    
   }
 }
